a797c0c9b941e21e9b00628fc4d380fecb5e6fe0
santech1995 <sanjanac777@gmail.com>
2022-11-20 13:07:18 -0800

    db changes

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var app = express();
+const csvtojson = require('csvtojson');
 
-// var mongoose = require("mongoose");
-// mongoose.connect();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root@123',
+    database: 'cmpe280'
+})
 
 app.get('/', function(req,res) {
     res.sendFile(__dirname+'/index.html');
 })
 
+connection.connect(function(error) {
+    if(error) throw error;
+    else {
+        console.log("Connected to Database");
+    }
+})
+  
 app.listen(3000);
